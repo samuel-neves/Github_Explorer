6d3de259395a137d42dee4948b885a45e3d50f7f
Samuel212584 <samuel212584@gmail.com>
2020-09-15 14:58:03 -0300

    route test

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -65,7 +65,7 @@ const Dashboard: React.FC = () => {
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
-      <Title>Explore repositórios no Github</Title>;
+      <Title>Explore repositórios no Github</Title>
       <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
