2212ac7e0af8d80a4cd38ba45589c931079df55e
Samuel212584 <samuel212584@gmail.com>
2020-09-15 14:53:23 -0300

    route correction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
+    <Route path="/Github_Explorer" exact component={Dashboard} />
     <Route path="/repository/:repository+" component={Repository} />
   </Switch>
 );
