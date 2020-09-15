ddceca1d246e5137c35ee322c45b4b6abe02ba56
Samuel212584 <samuel212584@gmail.com>
2020-09-15 12:51:59 -0300

    Updates

---
+++ precache-manifest.ca627f82828fd5f8ac160e2c79c890ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f323a91f79788b2bc0f27403e9d95b20",
+    "url": "/Github_Explorer/index.html"
+  },
+  {
+    "revision": "f1e753ab0cdb0b638e1c",
+    "url": "/Github_Explorer/static/js/2.cf16e1e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github_Explorer/static/js/2.cf16e1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a4fd411142b06c36c0",
+    "url": "/Github_Explorer/static/js/main.c4e17fc8.chunk.js"
+  },
+  {
+    "revision": "7173aed88077b0c83cb1",
+    "url": "/Github_Explorer/static/js/runtime-main.e176dd9a.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/Github_Explorer/static/media/github_background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/Github_Explorer/static/media/logo.04f6798f.svg"
+  }
+]);
